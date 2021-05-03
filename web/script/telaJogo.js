55c216a91fd4c3d07e4b57ff8ee7bc7a77e6dd3d
Isac Sousa <isacsousadev@gmail.com>
2021-05-03 20:21:16 -0300

    Refactor: Divisão de responsabilidades - arquivo para funções de background

--- web/script/telaJogo.js
+++ web/script/telaJogo.js
@@ -1,27 +1,12 @@
-const URL_NARUTO = "https://i.ibb.co/2YPMPcw/wp5845402.jpg";
-const CARD_COLOR_NARUTO = "linear-gradient(rgb(93, 94, 170), rgb(93, 66, 103))";
-const URL_HERO = "https://i.ibb.co/c2CsvNN/wallup-net.jpg";
-const CARD_COLOR_HERO = "#1780a1";
-const URL_FUTEBOL =
-  "https://i.ibb.co/JFZrNZt/Neymar-JR-Neymar-Paris-Saint-Germain-P-S-G-soccer-1389397.jpg";
-const CARD_COLOR_FUTEBOL = "#7b2cbf";
 const URL_NARUTO_DECK = "http://localhost:3000/narutoCards";
 const URL_HEROES_DECK = "http://localhost:3000/heroesCards";
 const URL_FUTEBOL_DECK = "http://localhost:3000/futebolCards";
-
-backgroundSelector();
-
-// document.addEventListener("DOMContentLoaded", async () => {
-//   cardFirstClick();
-// });
+let pontosJogador = 0;
+let pontosCpu = 0;
+let rounds = 1;
 
 document.addEventListener("DOMContentLoaded", async () => {
   cardFirstClick();
-
-  const attributes = document.querySelectorAll(".atr-player-card");
-  attributes.forEach((value) => {
-    value.addEventListener("click", attributeSelection);
-  });
 });
 
 async function getCards() {
@@ -101,12 +86,6 @@ function changeCardFutebol(card) {
   PlayerCard[5].innerHTML = `Fisico: ${card.physical}`;
 }
 
-function queryString() {
-  const urlParams = new URLSearchParams(location.search);
-  const deck = urlParams.get("deck");
-  return deck;
-}
-
 function cardFirstClick() {
   const element = document.getElementById("card-click");
   const elementCard = document.getElementById("card1");
@@ -119,10 +98,6 @@ function cardFirstClick() {
     value.addEventListener("click", attributeSelection);
   });
 }
-
-let pontosJogador = 0;
-let pontosCpu = 0;
-let rounds = 1;
 
 function updateRoud() {
   let roundsDiplay = document.querySelector("#tela-jogo__rodada");
@@ -203,25 +178,3 @@ function winMove(deckWinner, deckLoser) {
   deckWinner.push(deckLoser.shift);
 }
 
-function backgroundSelector() {
-  let deck = queryString();
-  let background = document.querySelector(".tela-jogo__container");
-  let backgroundCard = document.getElementsByClassName("tela-jogo__cards");
-
-  if (deck === "cardNaruto") {
-    background.style.backgroundImage = `url(${URL_NARUTO})`;
-    for (let i = 0; i < backgroundCard.length; i++) {
-      backgroundCard[i].style.background = CARD_COLOR_NARUTO;
-    }
-  } else if (deck === "cardHero") {
-    background.style.backgroundImage = `url(${URL_HERO})`;
-    for (let i = 0; i < backgroundCard.length; i++) {
-      backgroundCard[i].style.background = CARD_COLOR_HERO;
-    }
-  } else if (deck === "cardFutebol") {
-    background.style.backgroundImage = background.style.backgroundImage = `url(${URL_FUTEBOL})`;
-    for (let i = 0; i < backgroundCard.length; i++) {
-      backgroundCard[i].style.background = CARD_COLOR_FUTEBOL;
-    }
-  }
-}
