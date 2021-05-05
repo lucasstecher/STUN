32bde3e142614682faac63e771ef64be034c9920
Peterson Cabrini <petersoncabrini@hotmail.com>
2021-05-04 20:15:41 -0400

    feat/fix: adicionado menu hamburguer na barra de navegacao para dispositivos mobile

--- web/script/interfaceIndex.js
+++ web/script/interfaceIndex.js
@@ -24,3 +24,12 @@ function getName() {
   }
   return "Anônimo";
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
