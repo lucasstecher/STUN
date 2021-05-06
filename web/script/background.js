3c2c57e939cc8bd70e058c412db9db9c945922f1
Peterson Cabrini <petersoncabrini@hotmail.com>
2021-05-05 22:50:57 -0400

    fix: alteracao das cores dos cards

--- web/script/background.js
+++ web/script/background.js
@@ -1,16 +1,16 @@
 const URL_NARUTO = "https://i.ibb.co/2YPMPcw/wp5845402.jpg";
-const CARD_COLOR_NARUTO = "linear-gradient(rgb(93, 94, 170), rgb(93, 66, 103))";
+const CARD_COLOR_NARUTO = "#344265";
 const URL_HERO = "https://i.ibb.co/c2CsvNN/wallup-net.jpg";
-const CARD_COLOR_HERO = "#1780a1";
+const CARD_COLOR_HERO = "#9e1f1f";
 const URL_FUTEBOL =
-  "https://i.ibb.co/JFZrNZt/Neymar-JR-Neymar-Paris-Saint-Germain-P-S-G-soccer-1389397.jpg";
-const CARD_COLOR_FUTEBOL = "#7b2cbf";
-const ATTRIBUTE_FONT_COLOR_NARUTO = "#FFF"
-const ATTRIBUTE_BACKGROUND_COLOR_NARUTO = "#EE5223"
+  "https://wallpapercave.com/wp/wp4275524.jpg";
+const CARD_COLOR_FUTEBOL = "#122142";
+const ATTRIBUTE_FONT_COLOR_NARUTO = "#000"
+const ATTRIBUTE_BACKGROUND_COLOR_NARUTO = "#faf961"
 const ATTRIBUTE_FONT_COLOR_HERO = "#FFF"
-const ATTRIBUTE_BACKGROUND_COLOR_HERO = "#EE5223"
+const ATTRIBUTE_BACKGROUND_COLOR_HERO = "#175687"
 const ATTRIBUTE_FONT_COLOR_FUTEBOL = "#FFF"
-const ATTRIBUTE_BACKGROUND_COLOR_FUTEBOL = "#EE5223"
+const ATTRIBUTE_BACKGROUND_COLOR_FUTEBOL = "#5f8c55"
 
 //
 function queryString() {
